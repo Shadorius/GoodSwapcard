84304171b2408670548555005ffb4435f128fe54
geoffroy adnet <comateen_1@hotmail.com>
2018-12-04 11:12:16 +0100

    ModelClient by CrazyPush

---
+++ GoodSwapcard/ModelClient/HourTimeMC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelClient
+{
+    public class HourTimeMC
+    {
+        public int Id { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+    }
+}
