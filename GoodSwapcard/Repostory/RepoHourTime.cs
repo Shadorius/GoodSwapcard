b52332ba8f990f081b8a8c6e27749ae81ac62bae
Shadorius <axel_fontesse@hotmail.com>
2018-12-04 11:40:33 +0100

    Add Repo HourTime

--- GoodSwapcard/Repostory/RepoHourTime.cs
+++ GoodSwapcard/Repostory/RepoHourTime.cs
@@ -1,4 +1,5 @@
-﻿using ModelServer;
+﻿using DAL;
+using ModelServer;
 using Repostory.Repository;
 using System;
 using System.Collections.Generic;
@@ -10,29 +11,73 @@ namespace Repostory
 {
     public class RepoHourTime : IRepository<HourTimeMS, int>
     {
+        private Connexion _con = new Connexion();
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            string Query = "Delete from [HourTime] where [Id]=" + id;
+            bool QueryState = _con.Delete(Query);
+            return QueryState;
         }
 
         public HourTimeMS Get(int id)
         {
-            throw new NotImplementedException();
+            string Query = "Select * from [Evenement] where [Id] =" + id;
+            List<Dictionary<string, object>> Result = _con.getData(Query);
+            HourTimeMS CurrentHourTime = null;
+
+            if (Result.Count != 0)
+            {
+                CurrentHourTime = new HourTimeMS
+                {
+                    Id = (int)Result[0]["Id"],
+                    Hour = (int)Result[0]["Hour"],
+                    Minute = (int)Result[0]["Minute"]
+                };
+            }
+            return CurrentHourTime;
         }
 
         public List<HourTimeMS> GetAll()
         {
-            throw new NotImplementedException();
+            string Query = "Select * from [HourTime]";
+            List<Dictionary<string, object>> Result = _con.getData(Query);
+            List<HourTimeMS> CurrentList = new List<HourTimeMS>();
+            if (Result.Count != 0)
+            {
+                for (int i = 0; i < Result.Count; i++)
+                {
+                    HourTimeMS temp = new HourTimeMS
+                    {
+                        Id = (int)Result[i]["Id"],
+                        Hour = (int)Result[i]["Hour"],
+                        Minute = (int)Result[i]["Minute"]
+                    };
+                    CurrentList.Add(temp);
+                }
+            }
+            return CurrentList;
         }
 
         public bool Insert(HourTimeMS item)
         {
-            throw new NotImplementedException();
+            string Query = "INSERT INTO [HourTime] Values (";
+            Query += "" + item.Hour + ",";
+            Query += "" + item.Minute;
+            Query += ")";
+
+            bool QueryResult = _con.Insert(Query);
+            return QueryResult;
         }
 
         public bool Update(HourTimeMS item)
         {
-            throw new NotImplementedException();
+            string Query = "UPDATE [HourTime] SET";
+            Query += "[Hour] = " + item.Hour + "";
+            Query += ", [Minute] = " + item.Minute + "";
+            Query += " WHERE Id=" + item.Id;
+
+            bool QueryResult = _con.Insert(Query);
+            return QueryResult;
         }
     }
 }
