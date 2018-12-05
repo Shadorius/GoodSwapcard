de7a1cae6ff9ad8b1e0b46e822f185a619693815
Shadorius <axel_fontesse@hotmail.com>
2018-12-05 10:30:29 +0100

    add Notif BLR

---
+++ GoodSwapcard/BusinessLayer/BLRNotifications.cs
@@ -0,0 +1,40 @@
+﻿using ModelClient;
+using ModelServer;
+using Repostory.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class BLRNotifications : IRepository<NotificationsMC, int>
+    {
+
+
+        public NotificationsMC Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<NotificationsMC> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(NotificationsMC item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(NotificationsMC item)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Delete(int id)
+        {
+            return true;
+        }
+    }
+}
