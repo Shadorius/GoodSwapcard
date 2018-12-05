dab8e7ee24a09de71c15ca96f8ce2f23650f9a67
Bouillon jeremy <Bouillon_jeremy_21@hotmail.fr>
2018-12-05 12:21:52 +0100

    Ajout model user MVC

---
+++ GoodSwapcard/MVC/Models/Utilisateur.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Utilisateur
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Nom")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Nom")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name ="Mot de passe")]
+        public string PsW { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime? Birthdate { get; set; }
+    }
+}
