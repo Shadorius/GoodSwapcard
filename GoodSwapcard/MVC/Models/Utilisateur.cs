daabca9fe056f3edcaebf4fce8e1f0276398d84c
Bouillon jeremy <Bouillon_jeremy_21@hotmail.fr>
2018-12-12 17:03:14 +0100

    Validation ok

--- GoodSwapcard/MVC/Models/Utilisateur.cs
+++ GoodSwapcard/MVC/Models/Utilisateur.cs
@@ -10,15 +10,15 @@ namespace MVC.Models
     {
         public int Id { get; set; }
         [Required]
-        [RegularExpression(@"[a-zA-Z]{1,50}", ErrorMessage ="Le nom ne peut contenir que de 1 à 50 à  caracthère")]
-        [Display(Name = "Nome")]
+        [RegularExpression(@"[a-zA-Z]{1,50}", ErrorMessage ="Le nom ne peut contenir que de 1 à 50 caracthère")]
+        [Display(Name = "Nom")]
         public string LastName { get; set; }
         [Required]
-        [RegularExpression(@"[a-zA-Z]{1,50}")]
+        [RegularExpression(@"[a-zA-Z]{1,50}", ErrorMessage = "Le nom ne peut contenir que de 1 à 50 caracthère")]
         [Display(Name = "Prénom")]
         public string FirstName { get; set; }
         [Required]
-        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$", ErrorMessage ="coucou")] //https://codes-sources.commentcamarche.net/source/49715-validation-de-mot-de-passe
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$", ErrorMessage ="8 caractères minimum dont une maj, min, un carac spécial")] //https://codes-sources.commentcamarche.net/source/49715-validation-de-mot-de-passe
         [Display(Name ="Mot de passe")]
         [DataType(DataType.Password)]
         public string PsW { get; set; }
@@ -33,13 +33,13 @@ namespace MVC.Models
         [Compare(nameof(Email))]
         public string ConfirmEmail { get; set; }
         [Display(Name = "Numéro de tel")]
-        [RegularExpression(@"[\d]{5,15}")]
+        [RegularExpression(@"[\d]{5,15}", ErrorMessage = "Ne peut contenir que des décimaux")]
         public string Phone { get; set; }
         [Display(Name = "Date de naissance")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? Birthdate { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Veuillez choisir un statut")]
         public Statut statut { get; set; }
     }
 }
